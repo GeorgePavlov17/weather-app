d685cd0d5f08c064904a1efecf43bb2211a97d9c
Georgi Pavlov <georgipavlov17gmail.com>
2022-11-23 17:46:59 +0200

    search bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { City } from '../types/City';
 
 @Component({
@@ -6,9 +6,10 @@ import { City } from '../types/City';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
 
   public cities: City[] = [];
+  public citiesToShow: City[] = [];
   public searchShown: boolean = false;
 
   constructor() {}
@@ -16,15 +17,33 @@ export class HomePage implements OnInit {
   ngOnInit() {
     this.cities.push(
       <City>{name: 'London', weatherType: 'Cloudy', temperature: 3, image: 'https://cdn.londonandpartners.com/-/media/images/london/visit/campaigns/international-recovery-campaign/lets-do-london-related-international-640.jpeg?mw=640&hash=477E6FF9F31F292EE017E5BA63027C7054EB39A0'},
-      <City>{name: 'Sofia', weatherType: 'Sunny', temperature: 6, image: 'https://www.transfertaxi.net/wp-content/uploads/2019/05/sofia-airport-to-city-center.jpg'}
+      <City>{name: 'Sofia', weatherType: 'Sunny', temperature: 6, image: 'https://www.transfertaxi.net/wp-content/uploads/2019/05/sofia-airport-to-city-center.jpg'},
+      <City>{name: 'Paris', weatherType: 'Sunny', temperature: 7, image: 'https://viatravelers.com/wp-content/uploads/2020/12/parisatnight.jpeg'}
     );
+    this.citiesToShow = this.cities;
+  }
+
+  ngOnDestroy() {
+    this.cities = [];
   }
 
   toggleSearch() {
     this.searchShown = !this.searchShown;
   }
 
-  citySearch() {
-    
+  hideResults() {
+    this.searchShown = false;
+  }
+
+  searchTowns(event: any) {
+    if(event.target.value !== '') {
+      const filter = event.target.value.toLowerCase();
+
+      this.citiesToShow = this.cities.filter(c => {
+        return c.name.toLowerCase().indexOf(filter) > -1;
+      })
+    } else {
+      this.citiesToShow = this.cities;
+    }
   }
 }
